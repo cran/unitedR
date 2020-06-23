0dc5f8f6f8304dd77ad53664304bb3053315ed17
David Schindler <dv.schindler@gmail.com>
2020-06-23 14:40:02 +0000

    version 0.4

---
+++ R/overtime.R
@@ -0,0 +1,133 @@
+utils::globalVariables(c("result", "outcome"))
+###############################################
+# --------------------------------------------#
+# overtime                                    #
+# --------------------------------------------#
+###############################################
+
+# --------------------------------------------
+# Main Function for computing overtime results
+# --------------------------------------------
+
+#' Computing overtime results
+#' 
+#' Computes the final overtime outcome.
+#' 
+#' @inheritParams overview
+#'
+#' @return \code{list} with probabilities of final outcome.
+#' 
+#' 
+#' @export
+overtime <- function(chancesHome, chancesAway, probGoalHome, probGoalAway) {
+  
+  # compute all possible results
+  allResults <- expand.grid(goalsHome = 0:chancesHome, goalsAway = 0:chancesAway)
+  # compute probability of results
+  allResults$probability <- apply(allResults, 1, function(x) {
+    probHome <- dbinom(x[1], chancesHome, prob = probGoalHome)
+    probAway <- dbinom(x[2], chancesAway, prob = probGoalAway)
+    probHome * probAway
+  }
+  )
+  
+  allResults$result <- ifelse(allResults$goalsHome == allResults$goalsAway, "tiedProbability", 
+                              ifelse(allResults$goalsHome > allResults$goalsAway, 'winProbabilityHome',
+                                     'winProbabilityAway'))
+
+  out <- ddply(allResults, .(result), summarize, probability = sum(probability))
+  
+  list('tiedProbability' = max(c(out[out[,1] == 'tiedProbability', 2]), 0),
+       'winProbabilityHome' = max(c(out[out[,1] == 'winProbabilityHome', 2]), 0),
+       'winProbabilityAway' = max(c(out[out[,1] == 'winProbabilityAway', 2]), 0)
+  )
+}
+
+
+# --------------------------------------------
+# Main Function for computing overtime results
+# --------------------------------------------
+
+#' Computing outcome of penalty shootout
+#' 
+#' Computes outcome of a penalty shootout.
+#' 
+#' @param initial number of initial penalties (default 5)
+#' @inheritParams overview
+#'
+#' @return \code{list} with probabilities of final outcome (\code{winProbabilityHome}, 
+#' \code{winProbabilityAway}).
+#' 
+#' @export
+penaltyShootout <- function(probPenaltySaveHome, probPenaltySaveAway, initial=5) {
+  if(all(c(probPenaltySaveHome, probPenaltySaveAway) == 0)) {
+    probPenaltySaveHome <- probPenaltySaveAway <- 0.5
+  }
+  if(all(c(probPenaltySaveHome, probPenaltySaveAway) == 1)) {
+    probPenaltySaveHome <- probPenaltySaveAway <- 0.5
+  }
+  
+  qHome <- 1-probPenaltySaveHome
+  qAway <- 1-probPenaltySaveAway
+  
+  # possible paths
+  e <- vector('list', 0)
+  for (i in 0:(initial-1)) {
+    e[[i*2 + 1]] <- c(0, 1)
+    e[[i*2 + 2]] <- c(0, -1)
+  }
+  
+  paths <- expand.grid(e)
+  
+  pathsTrans <- t(apply(paths, 1, function(x) {
+    cumX <- cumsum(as.numeric(x))
+    # end of penalty kicks
+    v <- which(abs(cumX) >= c(initial+1, rep(initial:2, each = 2), 1))
+    end <- ifelse(length(v) > 0, v[1], 2*initial)
+    # replace values with -99
+    if (end < (2*initial)) {
+      x[(end+1):(2*initial)] <- -99
+    }
+    r <- ifelse(cumX[2*initial] > 0, "winHome",
+                ifelse(cumX[2*initial] == 0, "draw",
+                       'winAway'))
+    c(x, end, r)
+  }))
+  # colnames
+  colnames(pathsTrans) <- c(paste("Penalty", 1:(2*initial), sep = "_"), "end", "outcome")
+  # remove duplicates
+  pathsTransWoD <- pathsTrans[!duplicated(pathsTrans), ]
+  
+  pathsProbs <- pathsTransWoD
+  # rename cols
+  pathsProbs[pathsProbs == 0] <- "noGoal"
+  pathsProbs[pathsProbs %in% c(1, -1)] <- "Goal"
+  pathsProbs[, (0:(initial-1) * 2 + 1)][pathsProbs[, (0:(initial-1) * 2 + 1)] == 'noGoal'] <- qHome
+  pathsProbs[, (0:(initial-1) * 2 + 1)][pathsProbs[, (0:(initial-1) * 2 + 1)] == 'Goal'] <- probPenaltySaveHome
+  pathsProbs[, (1:initial)*2][pathsProbs[, (1:initial)*2] == 'noGoal'] <- qAway
+  pathsProbs[, (1:initial)*2][pathsProbs[, (1:initial)*2] == 'Goal'] <- probPenaltySaveAway
+  pathsProbs[pathsProbs == -99] <- 1    
+  
+  # fill pathsTrans with probabilites
+  probDist <- data.frame(t(apply(pathsProbs, 1, function(x) {
+    r <- prod(as.numeric(x[1:(2*initial)]))
+    c(x, r)
+  })))
+  
+  # colnames
+  colnames(probDist) <- c(paste("Penalty", 1:(2*initial), sep = "_"), "end", 'outcome', "probability")
+  
+  # summarized output after 5 penalties
+  outAfter5 <- ddply(probDist, .(outcome), summarize, probability = sum(as.numeric(as.character(probability))))
+  
+  # further penalties after draw
+  winHome <- probPenaltySaveHome/(probPenaltySaveHome + probPenaltySaveAway)
+  winAway <- 1 - winHome
+  
+  out <- list(winProbabilityHome = outAfter5$probability[outAfter5$outcome == 'winHome'] + outAfter5$probability[outAfter5$outcome == 'draw'] * winHome,
+              winProbabilityAway = outAfter5$probability[outAfter5$outcome == 'winAway'] + outAfter5$probability[outAfter5$outcome == 'draw'] * winAway)
+  
+  return(out)
+}
+
+  
--- R/unitedRoverview.R
+++ R/unitedRoverview.R
@@ -4,6 +4,8 @@
 #' used in the \code{unitedR} package.
 #'
 #' @param away away team (an object of the \code{S4}class \code{formation})
+#' @param chancesAway goalscoring chances of away team
+#' @param chancesHome goalscoring chances of home team
 #' @param DF \code{numeric} vector for the strengths of the players in the 
 #' defense
 #' @param formation object of the \code{S4}class \code{formation}
@@ -13,8 +15,11 @@
 #' for yellow cards dependent on the used hardness
 #' @param home home team (an object of the \code{S4}class \code{formation})
 #' @param homeAdv \code{numeric} vector of length five with integers for the used hardness
+#' @param L \code{list} with elements of class \code{formation}
 #' @param MF \code{numeric} vector for the strengths of the players in the 
 #' midfield
+#' @param overtime \code{logical}, if True overtime win probabilites are calculated. Only
+#' available if total hardness is zero or one.
 #' @param penaltyGoalProb probability of a goal by a singular penalty
 #' @param penaltyProb occurrence probability of a penalty
 #' @param posPenalties number of possible penalties in a game
@@ -22,6 +27,10 @@
 #' probability of preventing a goal by the goalkeeper
 #' @param preventGoalSW factor multiplicied with the strength of the SW for computing the 
 #' probability of preventing a goal by the sweeper
+#' @param probGoalAway probability of scoring a goal for away team
+#' @param probGoalHome probability of scoring a goal for home team
+#' @param probPenaltySaveAway probability of saving a penalty for away team
+#' @param probPenaltySaveHome probability of saving a penalty for home team 
 #' @param r number of replications for the simulation of hardness and penalties, can 
 #' be \code{missing} (exact results will be computed)
 #' @param ST \code{numeric} vector of integers for the strenghts of the strikers
